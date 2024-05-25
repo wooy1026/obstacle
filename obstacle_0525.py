fb3c81ff02ef2e4cf2cbae3da5f09825a74efe26
wooy1026 <126976981+wooy1026@users.noreply.github.com>
2024-05-25 17:44:42 +0900

    Update obstacle_0525.py

--- obstacle_0525.py
+++ obstacle_0525.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
-
 import rospy
 import math
 from geometry_msgs.msg import Twist
@@ -8,44 +7,73 @@ from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import Joy
 from std_msgs.msg import String
 
-class joy_drive:
+class obstacle_move:
     def __init__(self):
-		
         rospy.init_node('joy_drive')
-        rospy.Subscriber("/joy", Joy, self.send_joy)
+
+        #구독파트
+        rospy.Subscriber("/joy", Joy, self.send_joy_callback)
+        rospy.Subscriber("/scan", LaserScan, self.laser_callback)
+
+        #발행파트
         self.drive_pub = rospy.Publisher("cmd_vel_steer", Twist, queue_size=1)
+
+        #변수초기화
         self.auto_manaul = 0
         self.speed = 0
         self.steering = 0
         self.flag = 0
 
-    def send_joy(self, data):
+    def send_joy_callback(self, data): #joy_callback함수
         self.joy_btn = data.buttons
         self.joy_axes = data.axes
-        drive = Twist()
+        self.auto = self.joy_btn[4]
+        self.manual = self.joy_btn[5]
 
-        manaul = self.joy_btn[5]
-        # print(manaul)
-        if manaul == 1:
+    def drive_control(self, event): #실제 주행명령함수
+        drive = Twist()
+        
+        if self.manual == 1:
             self.speed = self.joy_axes[4]
-            # Twist에 int만 넣을 수 있어서 *10을 곱해서 보냄
-            # 아두이노 에서는 값을 10 나눠서 제어함	
-            self.steering = self.joy_axes[0]*30
-            drive.linear.x = float(self.speed)
-            drive.angular.z = float(self.steering)
+            self.steering = self.joy_axes[0]
+            drive.linear.x = self.speed*1
+            drive.angular.z = int(self.steering * 30)
+
+            
+        elif self.auto == 1:
+            stanley_steer_angle = self.stanley_control_angle()
+            if stanley_steer_angle is None: # 큰경우 정지
+                drive.linear.x = 0
+                drive.angular.z = 0
+            else:
+                drive.linear.x = 0.7
+                drive.angular.z = stanley_steer_angle
+
         else:
             drive.linear.x = 0
             drive.angular.z = 0
-            
-        print(drive.linear.x, drive.angular.z)
         self.drive_pub.publish(drive)
         
     def detect_obstacle(self, data):
         self.obstacle = data.data
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    ManualCar = joy_drive()
     try:
+        MoveCar = obstacle_move()
         rospy.spin()
     except KeyboardInterrupt:
-        print("program down")
+        print("Program terminated")
